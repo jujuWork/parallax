37095138fbdd88339f02d0a1d00920d0fb4a5caa
Joharry Sanchez <151762647+jujuWork@users.noreply.github.com>
2026-01-13 13:42:34 +0900

    dropdown animation

---
+++ assets/js/header-js/header.js
@@ -0,0 +1,31 @@
+// On click dropdown and image animation
+
+function toggleDropdown(event, dropdownId) {
+  event.preventDefault();
+  const dropdown = document.getElementById(dropdownId);
+  const button = event.currentTarget; //get the click <a>
+  const arrow = button.querySelector('.drop-arr'); //get the span inside
+
+  dropdown.classList.toggle('show');
+  arrow.classList.toggle('active'); /// Toggle the active class on the span
+}
+
+// Closing the dropdown menu if users click outside
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn') && !event.target.closest('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName('dropdown-content');
+    let arrows = document.getElementsByClassName('drop-arr'); // get all arrow span
+
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      // if the dropdown is open, close it bt removing the show class
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+
+    for (let i = 0; 0 < arrows.length; i++) {
+      arrows[i].classList.remove('active');
+    }
+  }
+};
